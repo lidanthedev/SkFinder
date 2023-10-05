5e15d405b16a0facbd6ce2a39de4b6910f097f16
Lidan <ls301205@gmail.com>
2023-10-05 23:50:20 +0300

    Created Project

---
+++ src/main/java/me/lidan/skfinder/SkFinder.java
@@ -0,0 +1,17 @@
+package me.lidan.skfinder;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SkFinder extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
