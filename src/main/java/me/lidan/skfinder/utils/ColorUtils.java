dab7a3c580ecd27016b9537813eaee27dc642879
lidanthedev <ls301205@gmail.com>
2023-10-08 17:04:29 +0300

    Revert "Better & Easier Utility"

--- src/main/java/me/lidan/skfinder/utils/ColorUtils.java
+++
@@ -1,9 +0,0 @@
-package me.lidan.skfinder.utils;
-
-import net.md_5.bungee.api.ChatColor;
-
-public class ColorUtils {
-    public static String trans(String str) {
-        return ChatColor.translateAlternativeColorCodes('&', str);
-    }
-}
